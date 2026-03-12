210f8fcf1b1cf8ed7e629e87d5c193e446cb26a1
unknown <joshuaoni@getfoodcourt.com>
2026-03-12 17:04:01 +0100

    realtime chat application

--- src/src/setupTests.ts
+++ src/src/setupTests.ts
@@ -1,3 +1,9 @@
 import "@testing-library/jest-dom";
 
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
+
+// Mock window.location.reload
+Object.defineProperty(window, "location", {
+    configurable: true,
+    value: { reload: jest.fn() },
+});
